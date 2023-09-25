7bb1eaeea311cc7cc2a09751be73ac44b7fb768e
kerriestauffer <kerrie.stauffer@bfh.ch>
2023-09-25 13:15:37 +0200

    1.2 replace template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
