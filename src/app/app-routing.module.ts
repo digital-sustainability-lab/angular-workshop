e8de18d65e39d02239558bd469a6062194724ef5
Aaron Sägesser <aaron.saegesser@bfh.ch>
2023-09-25 15:54:09 +0200

    2.3 routing in .ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   { path: 'overview', component: OverviewComponent },
+  { path: 'detail/:id', component: DetailComponent },
   { path: '**', redirectTo: 'overview' },
 ];
 
--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 export class OverviewComponent {
   label: string = 'this is a toggle';
   checked: boolean = true;
+
+  constructor(private router: Router) {}
+
+  navigateToDetail(id: number) {
+    this.router.navigate([`detail/${id}`]);
+  }
 }
