4a4549144bbf45dcf63ec220990781bdaf77ac28
Aaron Sägesser <aaron.saegesser@bfh.ch>
2023-09-25 15:53:21 +0200

    2.2 use router-outlet and routerlink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,25 @@ import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailComponent } from './detail/detail.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, OverviewComponent],
+  declarations: [
+    AppComponent,
+    OverviewComponent,
+    NavbarComponent,
+    DetailComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
